dc3ab10b395b30422b8ccee06ab24a1a0e93eb09
x250 <rizkynasrulloh08@gmail.com>
2024-07-02 21:44:16 +0700

    belajar scope variabels

---
+++ scopeVariabels.js
@@ -0,0 +1,11 @@
+var myLunch = "nasi bebek"
+var myLunch = "nasi bebek1"
+console.log(myLunch);
+
+const mobil = "mie sedap"
+// console.log(mobil);
+
+// console.log(meja);
+let meja = "mie ayam"
+// let meja = "bebek"
+// meja = "ikan goreng"
